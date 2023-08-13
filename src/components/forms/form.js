29a5bc5053bfd36d6bd8a4c636229223daaf7e50
Nikolayk Muñoz Alvarez <nikotrashy24072019@gmail.com>
2023-08-13 10:40:16 -0600

    configure the form with styles and configure new components

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,11 +1,12 @@
 import "./form.css";
+import TextCamp from "../textCamp/textCamp";
 
 const Form = () => {
   return (
     <section className='form'>
       <form className='react__form'>
         <h2 className='form__title'>Rellena el formulario para crear el colaborador</h2>
-
+        <TextCamp />
       </form>
     </section>);
 };
---
+++ src/components/textCamp/textCamp.js
@@ -0,0 +1,11 @@
+import './textCamp.css'
+const TextCamp = () => {
+    return (
+        <div className='textCamp'>
+            <label>Nombre</label>
+            <input placeholder='Ingresar nombre'/> 
+        </div>
+    );
+};
+
+export default TextCamp;
