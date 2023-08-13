fe7eb458280a46b79416df26e4f5eed61851163c
Nikolayk Muñoz Alvarez <nikotrashy24072019@gmail.com>
2023-08-13 02:29:21 -0600

    configuration from styles to the prject and create forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Form from "./components/forms/form";
 import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
     </div>
   );
 }
---
+++ src/components/forms/form.js
@@ -0,0 +1,13 @@
+import "./form.css";
+
+const Form = () => {
+  return (
+    <section className='form'>
+      <form className='react__form'>
+        <h2 className='form__title'>Rellena el formulario para crear el colaborador</h2>
+
+      </form>
+    </section>);
+};
+
+export default Form;
